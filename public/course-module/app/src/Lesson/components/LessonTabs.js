741288f28b9cbc6c3d26de6c4acb597a82c33386
Nathan Kleekamp <nkleekamp@gmail.com>
2016-12-14 15:46:20 -0500

    bringing in refactor branch of course-module

---
+++ public/course-module/app/src/Lesson/components/LessonTabs.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import shortid from 'shortid';
+
+const { string, array } = React.PropTypes;
+
+
+/*
+ * The LessonTabs component.
+ *
+ * @since 1.0.0
+ */
+
+const LessonTabs = React.createClass({
+  propTypes: {
+    description: string.isRequired,
+    transcript: string.isRequired,
+    resource: array
+  },
+
+
+  rawDescription() {
+    return {  __html: this.props.description };
+  },
+
+
+  rawTranscript() {
+    return { __html: this.props.transcript };
+  },
+
+
+  isEmpty(array) {
+    return (array.length === 0) ? true : false;
+  },
+
+
+  render() {
+    const resources = this.props.resources.map((resource) => {
+      return (
+        <li key={ shortid.generate() }><a href={ resource.src } >{ resource.title }</a></li>
+      );
+    });
+
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Transcript</Tab>
+          { this.isEmpty(this.props.resources) === true ? null : <Tab>Lesson Resources</Tab> }
+        </TabList>
+        <TabPanel>
+          <div dangerouslySetInnerHTML={ this.rawDescription() }></div>
+        </TabPanel>
+        <TabPanel>
+          <div dangerouslySetInnerHTML={ this.rawTranscript() }></div>
+        </TabPanel>
+        { this.isEmpty(this.props.resources) === true ? null : <TabPanel><ul>{ resources }</ul></TabPanel> }
+      </Tabs>
+    );
+  }
+});
+
+export default LessonTabs;
+
--- public/course-module/app/src/sessionStorage.js
+++ public/course-module/app/src/sessionStorage.js
@@ -1,21 +1,43 @@
+/*
+ * Check if the key 'state' exists in sessionStorage
+ *
+ * @todo Something more unique that 'state'
+ *
+ * @return {Object} - If 'state' exists, parse it and return the object
+ * @return {undefined} - If 'state' does not exist, return undefined
+ *
+ * @since 1.0.0
+ */
+
 export const loadState = () => {
 	try {
 		const serializedState = sessionStorage.getItem('state');  // change name of state to something less common
-			if (serializedState === null ) {
-				return undefined;
-			}
-			return JSON.parse(serializedState);
+
+    if (serializedState === null) {
+      return undefined;
+    }
+
+	  return JSON.parse(serializedState);
 	} catch (err) {
-			return undefined;
+	  return undefined;
 	}
 }
+
+
+
+
+/*
+ * Save state to sessionStorage
+ *
+ * @todo Something more unique that 'state'
+ *
+ * @since 1.0.0
+ */
 
 export const saveState = state => {
 	try {
 		const serializedState = JSON.stringify(state);
 		sessionStorage.setItem('state', serializedState)
-
-	} catch (err) {
-			//
-	}
+	} catch (err) {}
 }
+
