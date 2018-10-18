849968703165230ff97cc85c1c2717dabc0c704b
Edwin Mah <me@edwinmah.com>
2018-10-18 15:11:22 -0400

    Use var for wider browser compatibility

--- public/america-api-client-public.js
+++ public/america-api-client-public.js
@@ -2,9 +2,9 @@
  
   function initialize() {
     window.addEventListener('onReadyModule', function() {
-      let container = $( '.course-container' );
-      let placeholder = $('.course-placeholder');
-      let preloader = $( '.plugin-preloader' );
+      var container = $( '.course-container' );
+      var placeholder = $('.course-placeholder');
+      var preloader = $( '.plugin-preloader' );
       container.addClass('course-fade-in');
       placeholder.hide();
       preloader.hide();
